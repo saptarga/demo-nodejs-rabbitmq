28ebb424f63de8b62225fcffcb5365f3b408b67b
saptarga <saptarga@gmail.com>
2021-08-12 09:37:14 +0700

    Add Example Topic Exchange

--- order-service/src/services/mqService.js
+++ order-service/src/services/mqService.js
@@ -2,7 +2,7 @@ const amqp = require("amqplib");
 const { logger } = require("./loggerService");
 const MQ_HOST = process.env.MQ_HOST || "localhost"; // create MQ connection string using environment variable
 const MQ_URL = `amqp://${MQ_HOST}:5672`;
-const { EXCHANGE, QUEUE } = require("../statval/constants");
+const CONSTANT = require("../statval/constants");
 let orderChannel = null;
 
 /**
@@ -13,13 +13,25 @@ const amqpConnect = async () => {
 		const mqConnection = await amqp.connect(MQ_URL);
 		orderChannel = await mqConnection.createChannel();
 
-		await orderChannel.assertExchange(EXCHANGE, "fanout", {
+		await orderChannel.assertExchange(CONSTANT.EXCHANGE_FANOUT, "fanout", {
+			durable: false,
+		});
+
+		await orderChannel.assertExchange(CONSTANT.EXCHANGE_TOPIC, "topic", {
 			durable: false,
 		});
 
 		// Ensure that the queue exists or create one if it doesn't
-		await orderChannel.assertQueue(QUEUE);
-		await orderChannel.bindQueue(QUEUE, EXCHANGE, "");
+		await orderChannel.assertQueue(CONSTANT.QUEUE);
+		await orderChannel.bindQueue(CONSTANT.QUEUE, CONSTANT.EXCHANGE_FANOUT, "");
+
+		// Ensure that the queue exists or create one if it doesn't
+		await orderChannel.assertQueue(CONSTANT.QUEUE_TOPIC);
+		await orderChannel.bindQueue(
+			CONSTANT.QUEUE_TOPIC,
+			CONSTANT.EXCHANGE_TOPIC,
+			"orders"
+		);
 
 		logger.info(`AMQP - connection established at ${MQ_URL}`);
 	} catch (ex) {
@@ -33,7 +45,16 @@ const amqpConnect = async () => {
  * @param {Object} order - order object containing order details
  */
 const publishOrderToExchange = (order) => {
-	orderChannel.publish(EXCHANGE, "", Buffer.from(JSON.stringify(order)));
+	orderChannel.publish(
+		CONSTANT.EXCHANGE_FANOUT,
+		"",
+		Buffer.from(JSON.stringify(order))
+	);
+	orderChannel.publish(
+		CONSTANT.EXCHANGE_TOPIC,
+		"orders",
+		Buffer.from(JSON.stringify(order))
+	);
 	logger.info(`AMQP - order ${order._id} placed`);
 };
 
--- order-service/src/statval/constants.js
+++ order-service/src/statval/constants.js
@@ -1,5 +1,7 @@
-const EXCHANGE = "demo_order";
-const QUEUE = "demo_order.process";
+const EXCHANGE_FANOUT = "order";
+const EXCHANGE_TOPIC = "order_topic";
+const QUEUE = "order.process";
+const QUEUE_TOPIC = "order_topic.process";
 
 const ITEM_PRICE = {
 	burger: 50.0,
@@ -21,6 +23,8 @@ module.exports = {
 	MORGAN_CONFIG: MORGAN_CONFIG,
 	ORDER_SERVICE_WELCOME_MSG: ORDER_SERVICE_WELCOME_MSG,
 	ERROR_MAPPING: ERROR_MAPPING,
-	EXCHANGE: EXCHANGE,
+	EXCHANGE_FANOUT: EXCHANGE_FANOUT,
+	EXCHANGE_TOPIC: EXCHANGE_TOPIC,
 	QUEUE: QUEUE,
+	QUEUE_TOPIC: QUEUE_TOPIC,
 };
